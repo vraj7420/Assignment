7d3043ed6822b050de6e414ba75f44f69c9a42b7
LENOVO <vbrahmbhatt7420@gmail.com>
2022-01-07 13:41:24 +0530

    final

--- app/src/main/java/com/example/activityliifecycle/MainActivity.java
+++ app/src/main/java/com/example/activityliifecycle/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,11 +18,11 @@ import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
-      TextView tvActivityState;
-      ListView listOfActivityState;
-      ArrayList<String> state=new ArrayList<String>();
-      ArrayAdapter<String> arrayAdapter;
-      Button activityChnageBtn;
+    private   TextView tvActivityState;
+    private ListView listOfActivityState;
+    private   ArrayList<String> state=new ArrayList<String>();
+    private ArrayAdapter<String> arrayAdapter;
+    private Button activityChnageBtn;
 
 
     @Override
@@ -33,13 +34,7 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter= new ArrayAdapter<String>(this, R.layout.simple_list_layout1, state);
         activityChnageBtn=(Button)findViewById(R.id.activicyChangeBtn);
         String temp="OnCreateState";
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
-        String time=sdf.format(calendar.getTime());
-        String timeState=time+temp;
-        state.add(timeState);
-        tvActivityState.setText(timeState);
-        listOfActivityState.setAdapter(arrayAdapter);
+        setState(temp);
         activityChnageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,20 +49,24 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("list",state);
         startActivity(i);
     }
-
+     public  void setState(String temp)
+     {
+         Log.i("State",temp);
+         Calendar calendar=Calendar.getInstance();
+         SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
+         String time=sdf.format(calendar.getTime());
+         String timeState=time+temp;
+         activityChnageBtn=(Button)findViewById(R.id.activicyChangeBtn);
+         state.add(timeState);
+         tvActivityState.setText(timeState);
+         listOfActivityState.setAdapter(arrayAdapter);
+     }
 
     @Override
     protected void onStart() {
         super.onStart();
         String temp="OnStartState";
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
-        String time=sdf.format(calendar.getTime());
-        String timeState=time+temp;
-        activityChnageBtn=(Button)findViewById(R.id.activicyChangeBtn);
-        state.add(timeState);
-        tvActivityState.setText(timeState);
-        listOfActivityState.setAdapter(arrayAdapter);
+        setState(temp);
 
     }
 
@@ -75,13 +74,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         String temp="OnResumeState";
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
-        String time=sdf.format(calendar.getTime());
-        String timeState=time+temp;
-        state.add(timeState);
-        tvActivityState.setText(timeState);
-        listOfActivityState.setAdapter(arrayAdapter);
+        setState(temp);
 
     }
 
@@ -89,42 +82,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         String temp="OnPauseState";
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
-        String time=sdf.format(calendar.getTime());
-        String timeState=time+temp;
-        state.add(timeState);
-        tvActivityState.setText(timeState);
-        listOfActivityState.setAdapter(arrayAdapter);
-
+        setState(temp);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         String temp="OnStopState";
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
-        String time=sdf.format(calendar.getTime());
-        String timeState=time+temp;
-        state.add(timeState);
-        tvActivityState.setText(timeState);
-        listOfActivityState.setAdapter(arrayAdapter);
-
+        setState(temp);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        String temp="OnDesteoyState";
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm:ss");
-        String time=sdf.format(calendar.getTime());
-        String timeState=time+temp;
-        state.add(timeState);
-        tvActivityState.setText(timeState);
-        listOfActivityState.setAdapter(arrayAdapter);
-
+        String temp="OnDestroyState";
+        setState(temp);
     }
-
 }
--- app/src/main/java/com/example/activityliifecycle/Second_Activity.java
+++ app/src/main/java/com/example/activityliifecycle/Second_Activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,9 +13,9 @@ import android.widget.GridView;
 import java.util.ArrayList;
 
 public class Second_Activity extends AppCompatActivity {
-    GridView grid;
-    Button btnShare;
-    ArrayList<String> stateScondActivity;
+   private GridView grid;
+   private Button btnShare;
+   private ArrayList<String> stateSecondActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,21 +23,24 @@ public class Second_Activity extends AppCompatActivity {
         grid=(GridView)findViewById(R.id.grid);
         btnShare=(Button)findViewById(R.id.btnShare) ;
         Intent i=getIntent();
-        stateScondActivity= i.getStringArrayListExtra("list");
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.grid_view,stateScondActivity);
+        stateSecondActivity= i.getStringArrayListExtra("list");
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.grid_view,stateSecondActivity);
         grid.setAdapter(adapter);
         btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("Function","OnClick Call");
                 Intent i=new Intent(Intent.ACTION_SEND);
                 i.setType("text/plain");
                 String listString="";
-                for (String s :stateScondActivity)
+                for (String s :stateSecondActivity)
                 {
                     listString += s + "\t";
                 }
+                Log.i("Function","Staring ArrayList Converted in String");
                 i.putExtra(Intent.EXTRA_TEXT,listString);
                 startActivity(i);
+                Log.i("Function","Start Activity");
 
             }
         });
